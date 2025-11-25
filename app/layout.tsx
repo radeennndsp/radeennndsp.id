22bcf910089bad86d47f05c54776bf221ee80418
radeennndsp_ <radendewasaktiapurnama17@gmail.com>
2025-11-26 01:59:44 +0700

    fix vercel url env

--- app/layout.tsx
+++ app/layout.tsx
@@ -16,7 +16,7 @@ import { onestSans } from "@/common/styles/fonts";
 const appUrl =
   process.env.NODE_ENV === "development"
     ? "http://localhost:3000"
-    : `https://${process.env.VERCEL_PROJECT_PRODUCTION_URL}`;
+    : `https://${process.env.VERCEL_URL}`;
 
 export const metadata: Metadata = {
   metadataBase: new URL(appUrl),
